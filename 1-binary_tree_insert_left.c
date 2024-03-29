0043524cce43f86e3f8b687978e33a03889401c0
Z-bot-pi <xulubanazintle@gmail.com>
2024-03-29 09:24:42 -0700

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,50 +1,32 @@
 #include <stdlib.h>
 
-/* Structure for the binary tree node */
-typedef struct binary_tree_s {
-    int value;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-} binary_tree_t;
 
-/* Function to create a binary tree node */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
-    /* Allocate memory for the new node */
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL) {
-        return NULL;  // Return NULL on failure
-    }
-    
-    /* Initialize the new node */
-    new_node->value = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    
-    return new_node;  // Return a pointer to the new node
-}
+/* Function inserts a node as the left-child of another node */
 
-/* Function to insert a node as the left-child of another node */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
-    if (parent == NULL) {
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    if (parent == NULL) 
+    {
         return NULL;  // Cannot insert if parent is NULL
     }
     
-    /* Create a new node with the given value */
+    /* Creates new node with given value */
     binary_tree_t *new_node = binary_tree_node(parent, value);
-    if (new_node == NULL) {
-        return NULL;  // Return NULL on failure to create the new node
+    if (new_node == NULL)
+
+    {
+        return NULL;  // Return NULL when there's failure for creating new node
     }
     
-    /* If parent already has a left child, update its left child */
-    if (parent->left != NULL) {
+    /* If theres already a left child, update the left child */
+    if (parent->left != NULL)
+    {
         parent->left->parent = new_node;
         new_node->left = parent->left;
     }
     
-    /* Set the new node as the left child of the parent */
+    /* Sets new node to be the left child */
     parent->left = new_node;
     
-    return new_node;  // Return a pointer to the created node
+    return (new_node);  // Return a pointer to the created node
 }
